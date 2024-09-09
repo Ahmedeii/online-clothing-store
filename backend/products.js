428c649d90fa399c92d99ebe7d6aec4bc59b08e0
Ahmedeii <ahmedabdalnabe77@gmail.com>
2024-09-09 21:56:54 +0300

    first commit

---
+++ backend/products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink: ["https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226936/photo2/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_qqjylc.png"
+    ,"https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226936/photo2/1/NASA_Space_Bear_Print_T-Shirt_dj3jz2.jpg",
+    "https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226933/photo2/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_k6eqli.jpg",
+    "https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226931/photo2/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_1_dykx39.jpg",
+  
+    ],
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink: ["https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226946/photo2/2/1658329192737168ff6ad063452563108e1de4d132_thumbnail_900x_uryax1.webp","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226943/photo2/2/164802324294a9e00cd18e3bdacdb07faadc6ea08e_thumbnail_900x_bwjbvw.webp","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226941/photo2/2/16480232420809ac54cac58a758d88fbee070f7f7a_thumbnail_900x_wpdfsb.webp","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226940/photo2/2/1648023235ba32327d7aecda5b9de9913efa316d9e_thumbnail_900x_b5q8xv.webp"],
+
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+
+  imageLink: ["https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226950/photo2/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_White___XL_w7fbvc.png","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226950/photo2/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Khaki___XL_chnsaq.png","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226949/photo2/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Grey___XL_weagdx.png","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226946/photo2/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Black___XL_xfepur.png"],
+
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink: ["https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226931/photo2/4/1646880634f3ecf23038717f7adbcf3658effab2e7_thumbnail_900x_zo7bgh.webp","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226930/photo2/4/1646822799c97e5f9754d3b3314b4e6ffd848ad22d_thumbnail_900x_cmjmgd.webp","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226928/photo2/4/164430580182db3b5a5192e660a87c832a397044b6_thumbnail_900x_cl9vau.webp","https://res.cloudinary.com/ddtq3oqmd/image/upload/v1711226926/photo2/4/163826656927114e9da27ea115f55404141ba11264_thumbnail_900x_t4bzh3.webp"],
+
+  },
+ ];
+ module.exports = products;
---
+++ frontend/src/MUI-components/Appbar.jsx
@@ -0,0 +1,54 @@
+ 
+import {
+  Toolbar,
+
+  Avatar,
+  Link,
+  Typography,
+  IconButton,
+  AppBar,
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+const Appbar = ({ drawerWidth, showDrawer }) => {
+  return (
+    <AppBar
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { xs: 0, sm: `${drawerWidth}px` },
+      }}
+      position="static"
+    >
+      <Toolbar
+  >
+        <IconButton
+          onClick={() => {
+            showDrawer();
+          }}
+          sx={{ mr: "9px", display: { sm: "none" } }}
+        >
+          <Menu />
+        </IconButton>
+        <Link
+          sx={{
+            flexGrow: 1,
+            textDecoration: "none",
+            "&:hover": { fontSize: "16.5px" },
+          }}
+          color="inherit"
+          href="/"
+        >
+        Online store
+        </Link>
+
+        <Typography mr={2} variant="body1" color="inherit">
+          Ali Hassan
+        </Typography>
+
+        <Avatar alt="Remy Sharp" src="./imges/Ahmed.jpg" />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Appbar;
